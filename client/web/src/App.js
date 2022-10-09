1321c85a4ff38cb2db70001142a1db5c9b236788
Damian Perera <damianperera@outlook.com>
2022-10-09 20:06:57 +0200

    removed text

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -6,17 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <form action="https://www.google.com/search" class="searchform" method="get" name="searchform">
           <input autocomplete="on" class="form-control search" name="q" placeholder="Search Google or type a URL" required="required"  type="text" />
           <button class="button" type="submit">Search</button>
