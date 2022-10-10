06159c55384066e91a514e79d9b41346c3849f52
Damian Perera <damianperera@outlook.com>
2022-10-10 17:23:51 +0200

    used top bar

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -4,44 +4,78 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './App.css';
 import * as React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 import { Google, Code } from '@mui/icons-material';
-import { Button, Box, TextField, FormControl, Stack } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Box, TextField, FormControl, Stack , AppBar, Toolbar, IconButton, CssBaseline, MenuItem, InputAdornment } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
+
+  const [searchEngine, setSearchEngine] = React.useState('https://www.google.com/search');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSearchEngine(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Box
-          sx={{
-            width: 500,
-            height: 130,
-            backgroundColor: 'white',
-            padding: 2,
-            display: 'flex',
-            borderRadius: 1.5
-          }}
-        >
-          <Stack direction="column" spacing={2}>
-            <form action="https://www.google.com/search">
-              <FormControl fullWidth>
-                <Stack direction="row" spacing={2}>
-                  <TextField autoFocus name="q" required id="search" label="Search Google" variant="outlined" sx={{ width: 385 }} />
-                  <Button variant="contained" type="submit" sx={{ width: 100 }} startIcon={<Google />}>Search</Button>
-                </Stack>
-              </FormControl>
-            </form>
-            <form action="https://stackoverflow.com/search">
-              <FormControl fullWidth>
-                <Stack direction="row" spacing={2}>
-                  <TextField name="q" required id="search" label="Search StackOverflow" variant="outlined" sx={{ width: 385 }} />
-                  <Button variant="contained" type="submit" sx={{ width: 100 }} startIcon={<Code />}>Search</Button>
-                </Stack>
-              </FormControl>
-            </form>
-          </Stack>
-        </Box>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+        <AppBar position="static">
+          <Toolbar sx={{ height: 100 }}>
+            <Box 
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              sx={{ flexGrow: 1 }}
+            >
+              <form action={searchEngine}>
+                <FormControl variant="standard">
+                  <TextField autoFocus name="q" required id="search" placeholder="Enter search text" variant="outlined" sx={{ width: 700 }} 
+                    InputProps={{
+                      startAdornment: 
+                        <InputAdornment>
+                          <Select 
+                            labelId="search-engine-label" 
+                            id="search-engine" 
+                            value={searchEngine} 
+                            onChange={handleChange} 
+                            sx={{
+                              boxShadow: 'none',
+                              '.MuiOutlinedInput-notchedOutline': { border: 0 } ,
+                              marginLeft: -2,
+                              marginRight: 2,
+                              width: 190
+                            }}
+                          >
+                            <MenuItem value={"https://www.google.com/search"}>
+                              <Stack direction="row" alignItems="center" gap={1}>
+                                <Google /> Google
+                              </Stack>
+                            </MenuItem>
+                            <MenuItem value={"https://stackoverflow.com/search"}>
+                              <Stack direction="row" alignItems="center" gap={1}>
+                                <Code /> StackOverflow
+                              </Stack>    
+                            </MenuItem>
+                          </Select>
+                        </InputAdornment>,
+                      endAdornment: 
+                          <IconButton type="submit" aria-label="search" size="small"><SearchIcon /></IconButton>
+                    }}
+                    />
+                </FormControl>
+              </form>
+            </Box>
+          </Toolbar>
+        </AppBar>
+    </ThemeProvider>
   );
 }
 
