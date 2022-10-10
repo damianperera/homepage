9f1500c25d48e6ea247c54f8419ccb62b7de54ef
Damian Perera <damianperera@outlook.com>
2022-10-10 07:18:37 +0200

    added autofocus

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <form action="https://www.google.com/search" class="searchform" method="get" name="searchform">
-          <input autocomplete="on" class="form-control search" name="q" placeholder="Search Google or type a URL" required="required"  type="text" />
+          <input autoFocus autocomplete="on" class="form-control search" name="q" placeholder="Search Google or type a URL" required="required"  type="text" />
           <button class="button" type="submit">Search</button>
         </form>
       </header>
