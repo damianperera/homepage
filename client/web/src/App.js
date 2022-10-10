faf8801fe55a1a473dbfae269f1e4eb770c05672
Damian Perera <damianperera@outlook.com>
2022-10-10 17:54:38 +0200

    remove magic strings

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -1,26 +1,28 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './App.css';
-import * as React from 'react';
-import SearchIcon from '@mui/icons-material/Search';
-import { Google, Code } from '@mui/icons-material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Box, TextField, FormControl, Stack , AppBar, Toolbar, IconButton, CssBaseline, MenuItem, InputAdornment } from '@mui/material';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
+import * as React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { Google, Code } from "@mui/icons-material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Box, TextField, FormControl, Stack , AppBar, Toolbar, IconButton, CssBaseline, MenuItem, InputAdornment, Select } from "@mui/material";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
 function App() {
 
-  const [searchEngine, setSearchEngine] = React.useState('https://www.google.com/search');
+  const googleSearchURL = "https://www.google.com/search";
+  const stackOverflowSearchURL = "https://stackoverflow.com/search";
 
-  const handleChange = (event: SelectChangeEvent) => {
+  const [searchEngine, setSearchEngine] = React.useState(googleSearchURL);
+
+  const handleChange = (event) => {
     setSearchEngine(event.target.value);
   };
 
@@ -28,7 +30,7 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
         <AppBar position="static">
-          <Toolbar sx={{ height: 100 }}>
+          <Toolbar sx={{ height: "10vh" }}>
             <Box 
               display="flex"
               justifyContent="center"
@@ -37,7 +39,15 @@ function App() {
             >
               <form action={searchEngine}>
                 <FormControl variant="standard">
-                  <TextField autoFocus name="q" required id="search" placeholder="Enter search text" variant="outlined" sx={{ width: 700 }} 
+                  <TextField autoFocus name="q" required id="search" placeholder="Enter search text" variant="outlined" 
+                    sx={{ 
+                      width: "70vh",
+                      "& .MuiOutlinedInput-root.Mui-focused": {
+                        "& > fieldset": {
+                          border: 0
+                        }
+                      }
+                    }} 
                     InputProps={{
                       startAdornment: 
                         <InputAdornment>
@@ -45,21 +55,21 @@ function App() {
                             labelId="search-engine-label" 
                             id="search-engine" 
                             value={searchEngine} 
-                            onChange={handleChange} 
+                            onChange={handleChange}
                             sx={{
-                              boxShadow: 'none',
-                              '.MuiOutlinedInput-notchedOutline': { border: 0 } ,
+                              boxShadow: "none",
+                              ".MuiOutlinedInput-notchedOutline": { border: 0 },
                               marginLeft: -2,
                               marginRight: 2,
-                              width: 190
+                              width: "19vh"
                             }}
                           >
-                            <MenuItem value={"https://www.google.com/search"}>
+                            <MenuItem value={googleSearchURL}>
                               <Stack direction="row" alignItems="center" gap={1}>
                                 <Google /> Google
                               </Stack>
                             </MenuItem>
-                            <MenuItem value={"https://stackoverflow.com/search"}>
+                            <MenuItem value={stackOverflowSearchURL}>
                               <Stack direction="row" alignItems="center" gap={1}>
                                 <Code /> StackOverflow
                               </Stack>    
