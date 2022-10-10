2d15b626d01f65256413449d74ed74716cb42b8b
Damian Perera <damianperera@outlook.com>
2022-10-10 08:04:50 +0200

    added styles

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -2,29 +2,29 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Button from '@mui/material/Button';
 import './App.css';
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import FormControl from '@mui/material/FormControl';
+import { Button, Box, TextField, FormControl, Stack} from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Box
-          component="form"
           sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
+            width: 500,
+            height: 55,
+            backgroundColor: 'white',
+            padding: 2,
+            display: 'flex'
           }}
-          noValidate
-          autoComplete="on"
         >
           <form action="https://www.google.com/search">
             <FormControl focused="true">
-              <TextField autoFocus name="q" required id="outlined-basic" label="Search Google or type a URL" variant="filled" />
-              <Button variant="contained" type="submit">Search</Button>
+              <Stack direction="row" spacing={2}>
+                <TextField autoFocus name="q" required id="search" label="Search Google or type a URL" variant="filled" sx={{ width: 385 }} />
+                <Button variant="contained" type="submit" sx={{ width: 100 }}>Search</Button>
+              </Stack>
             </FormControl>
           </form>
         </Box>
