3b2e4a6b18e66dcab5ba10b10c0242fb086d6d4d
Damian Perera <damianperera@outlook.com>
2022-10-14 09:14:34 +0200

    moved SearchEngineBar to own file

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -4,82 +4,12 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./App.css";
 import * as React from "react";
-import { Google, Code, PrivacyTip, Newspaper, Search, Menu, Twitter, Error } from "@mui/icons-material";
+import { Newspaper, Menu, Twitter, Error } from "@mui/icons-material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Box, TextField, FormControl, Stack , AppBar, Toolbar, IconButton, CssBaseline, MenuItem, InputAdornment, Select, Grid, CircularProgress } from "@mui/material";
+import { Box, Stack , AppBar, Toolbar, IconButton, CssBaseline, Grid, CircularProgress } from "@mui/material";
 import { StyledItem, StyledDataGrid } from "./common/StyledComponents";
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
-import MainMenuDrawer from "./components/MainMenuDrawer";
-
-function SearchEngineBar() {
-  const googleSearchURL = "https://www.google.com/search";
-  const stackOverflowSearchURL = "https://stackoverflow.com/search";
-  const duckDuckGoSearchURL = "https://duckduckgo.com";
-
-  const [searchEngine, setSearchEngine] = React.useState(googleSearchURL);
-
-  const handleSearchEngineChange = (event) => {
-    setSearchEngine(event.target.value);
-  };
-
-  return (
-    <form action={searchEngine}>
-      <FormControl variant="standard">
-        <TextField autoFocus name="q" required id="search" placeholder="Enter search text" variant="outlined" 
-          sx={{ 
-            width: "70vh",
-            "& .MuiOutlinedInput-root.Mui-focused": {
-              "& > fieldset": {
-                border: 0
-              }
-            }
-          }} 
-          InputProps={{
-            startAdornment: 
-              <InputAdornment position="start">
-                <Select 
-                  labelId="search-engine-label" 
-                  id="search-engine" 
-                  value={searchEngine} 
-                  onChange={handleSearchEngineChange}
-                  sx={{
-                    boxShadow: "none",
-                    ".MuiOutlinedInput-notchedOutline": { border: 0 },
-                    "& .MuiOutlinedInput-root.Mui-focused": {
-                      "& > fieldset": {
-                        border: 0
-                      }
-                    },
-                    marginLeft: -2,
-                    marginRight: 2,
-                    width: "19vh"
-                  }}
-                >
-                  <MenuItem value={googleSearchURL}>
-                    <Stack direction="row" alignItems="center" gap={1}>
-                      <Google /> Google
-                    </Stack>
-                  </MenuItem>
-                  <MenuItem value={stackOverflowSearchURL}>
-                    <Stack direction="row" alignItems="center" gap={1}>
-                      <Code /> StackOverflow
-                    </Stack>    
-                  </MenuItem>
-                  <MenuItem value={duckDuckGoSearchURL}>
-                    <Stack direction="row" alignItems="center" gap={1}>
-                      <PrivacyTip /> DuckDuckGo
-                    </Stack>    
-                  </MenuItem>
-                </Select>
-              </InputAdornment>,
-            endAdornment: 
-              <IconButton type="submit" aria-label="search" size="small"><Search /></IconButton>
-          }}
-          />
-      </FormControl>
-    </form>
-  );
-}
+import { MainMenuDrawer, SearchEngineBar } from "./components";
 
 function Header() {
   const [menuToggle, setMenuToggle] = React.useState(false);
---
+++ client/web/src/components/index.js
@@ -0,0 +1,2 @@
+export { default as MainMenuDrawer } from "./MainMenuDrawer";
+export { default as SearchEngineBar } from "./SearchEngineBar";
