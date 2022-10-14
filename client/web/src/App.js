e31bb78e1820d1b530843341be7a907981a130ab
Damian Perera <damianperera@outlook.com>
2022-10-14 09:09:52 +0200

    moved MainMenuDrawer to own file

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -4,66 +4,12 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./App.css";
 import * as React from "react";
-import { Google, Code, PrivacyTip, Newspaper, Search, Menu, KeyRounded, GitHub, Twitter, Error } from "@mui/icons-material";
+import { Google, Code, PrivacyTip, Newspaper, Search, Menu, Twitter, Error } from "@mui/icons-material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Box, TextField, FormControl, Stack , AppBar, Toolbar, IconButton, CssBaseline, MenuItem, InputAdornment, Select, Grid, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, CircularProgress } from "@mui/material";
+import { Box, TextField, FormControl, Stack , AppBar, Toolbar, IconButton, CssBaseline, MenuItem, InputAdornment, Select, Grid, CircularProgress } from "@mui/material";
 import { StyledItem, StyledDataGrid } from "./common/StyledComponents";
-
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
-
-function MainMenuDrawer({ isOpen, onChange }) {
-  const licenseURL = "https://github.com/damianperera/homepage/blob/main/LICENSE.md";
-  const repositoryURL = "https://github.com/damianperera/homepage";
-
-  const handleLicenseClick = () => {
-    window.open(licenseURL, "_blank", "noopener,noreferrer");
-  }
-
-  const handleRepositoryClick = () => {
-    window.open(repositoryURL, "_blank", "noopener,noreferrer");
-  }
-  return (
-    <Drawer
-      open={isOpen}
-      onClose={onChange(false)}
-    >
-      <Box 
-        sx={{ width: 200 }}
-        role="presentation"
-        onClick={onChange(false)}
-        onKeyDown={onChange(false)}
-      >
-        <ListItemText primary="Home" secondary="Dev Homepage" sx={{ marginLeft: 1, ".MuiListItemText-primary": { fontWeight: "bold" } }} />
-        <Divider />
-        <Box 
-          sx={{
-            position: "fixed",
-            bottom: 0,
-            width: 200
-          }}
-        >
-          <Divider />
-          <ListItem key="repository" disablePadding>
-            <ListItemButton onClick={handleRepositoryClick}>
-              <ListItemIcon>
-                <GitHub />
-              </ListItemIcon>
-              <ListItemText primary="Repository" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem key="license" disablePadding>
-            <ListItemButton onClick={handleLicenseClick}>
-              <ListItemIcon>
-                <KeyRounded />
-              </ListItemIcon>
-              <ListItemText primary="License" />
-            </ListItemButton>
-          </ListItem>
-        </Box>
-      </Box>
-    </Drawer>
-  );
-}
+import MainMenuDrawer from "./components/MainMenuDrawer";
 
 function SearchEngineBar() {
   const googleSearchURL = "https://www.google.com/search";
---
+++ client/web/src/components/MainMenuDrawer.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { KeyRounded, GitHub } from "@mui/icons-material";
+import { Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from "@mui/material";
+
+function MainMenuDrawer({ isOpen, onChange }) {
+    const licenseURL = "https://github.com/damianperera/homepage/blob/main/LICENSE.md";
+    const repositoryURL = "https://github.com/damianperera/homepage";
+
+    const handleLicenseClick = () => {
+        window.open(licenseURL, "_blank", "noopener,noreferrer");
+    }
+
+    const handleRepositoryClick = () => {
+        window.open(repositoryURL, "_blank", "noopener,noreferrer");
+    }
+    return (
+        <Drawer
+        open={isOpen}
+        onClose={onChange(false)}
+        >
+        <Box 
+            sx={{ width: 200 }}
+            role="presentation"
+            onClick={onChange(false)}
+            onKeyDown={onChange(false)}
+        >
+            <ListItemText primary="Home" secondary="Dev Homepage" sx={{ marginLeft: 1, ".MuiListItemText-primary": { fontWeight: "bold" } }} />
+            <Divider />
+            <Box 
+            sx={{
+                position: "fixed",
+                bottom: 0,
+                width: 200
+            }}
+            >
+            <Divider />
+            <ListItem key="repository" disablePadding>
+                <ListItemButton onClick={handleRepositoryClick}>
+                <ListItemIcon>
+                    <GitHub />
+                </ListItemIcon>
+                <ListItemText primary="Repository" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem key="license" disablePadding>
+                <ListItemButton onClick={handleLicenseClick}>
+                <ListItemIcon>
+                    <KeyRounded />
+                </ListItemIcon>
+                <ListItemText primary="License" />
+                </ListItemButton>
+            </ListItem>
+            </Box>
+        </Box>
+        </Drawer>
+    );
+}
+
+export default MainMenuDrawer;
