829a8d941f44b01c4819b9a4ce45410d61fb825e
Damian Perera <damianperera@outlook.com>
2022-10-18 15:06:16 +0200

    updated test name

--- client/web/src/App.test.js
+++ client/web/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import App from "./App"
 
-test("renders learn react link", () => {
+test("renders search engines", () => {
 	render(<App />)
 	const linkElement = screen.getByText(/Google/i)
 	expect(linkElement).toBeInTheDocument()
