1a8eac41358085e3c76a445d6a94f4410605d757
Damian Perera <damianperera@outlook.com>
2023-06-28 22:03:56 +0200

    moved snackbar to top right

--- client/web/src/common/Alert.js
+++ client/web/src/common/Alert.js
@@ -1,7 +1,12 @@
 import { Snackbar, Alert as MuiAlert } from "@mui/material"
 
 export const Alert = ({ message, open, onClose, severity, autoHideDuration = 6000 }) => (
-	<Snackbar open={open} autoHideDuration={autoHideDuration} onClose={onClose}>
+	<Snackbar
+		open={open}
+		autoHideDuration={autoHideDuration}
+		onClose={onClose}
+		anchorOrigin={{ vertical: "top", horizontal: "right" }}
+	>
 		<MuiAlert onClose={onClose} severity={severity} sx={{ width: "100%" }}>
 			{message}
 		</MuiAlert>
